9643756620167e8d045d926878e0a0f2fe3ef22e
Sameep2808 <sameepote2808@gmail.com>
2021-12-07 17:06:33 -0500

    Improving ik

--- src/ikt.py
+++ src/ikt.py
@@ -51,22 +51,23 @@ def solve(T):
 
 def getqdot(J,xc,yc,zc,xd,yd,zd):
 	J_inv=np.linalg.pinv(J)
-	Xdot=sy.Matrix([[xd-xc,yd-yc,zd-zc]])
+	print(J_inv)
+	Xdot=sy.Matrix([[xd-xc,yd-yc,zd-zc,0,0,0]])
 	Xdot=np.array(Xdot,dtype='float')
-	Xdot_f=np.zeros((Xdot.shape[0],2*Xdot.shape[1]))
-	Xdot_f[:,:3]=Xdot
-	Qdot=J_inv@Xdot_f.T
+	#Xdot_f=np.zeros((Xdot.shape[0],2*Xdot.shape[1]))
+	#Xdot_f[:,:3]=Xdot
+	Qdot=J_inv@Xdot.T
 	Qdot=Qdot.T
 	Qdot=np.array(Qdot,dtype=float)
 	return Qdot
 
 def Inverse_kin(Q,T):
 	f=T[:3,3]
-	X=[theta1,theta2,theta3,theta4,theta5,theta6]
+	X=[theta1,theta2,theta3,theta4,theta5]
 	J_half=f.jacobian(X)
-	J_otherhalf=T0_1[:3,2].row_join(T0_2[:3,2].row_join(T0_3[:3,2].row_join(T0_4[:3,2].row_join(T0_5[:3,2].row_join(T0_6[:3,2])))))
+	J_otherhalf=T0_1[:3,2].row_join(T0_2[:3,2].row_join(T0_3[:3,2].row_join(T0_4[:3,2].row_join(T0_5[:3,2]))))
 	J=J_half.col_join(J_otherhalf)
-	J=J.subs({theta1:Q[0,0],theta2:Q[0,1],theta3:Q[0,2],theta4:Q[0,3],theta5:Q[0,4],theta6:Q[0,5]})
+	J=J.subs({theta1:Q[0,0],theta2:Q[0,1],theta3:Q[0,2],theta4:Q[0,3],theta5:Q[0,4]})
 	J=nsimplify(J,tolerance=1e-3,rational=True)
 	J=np.array(J,dtype=float)
 	return J
@@ -79,18 +80,18 @@ if __name__=="__main__":
 		         [0, 0, 0, 1]])
 	
 
-	A0_1= A.subs({alpha:pi/2, a:0, theta:theta1, d:355.6}) #0.3556
+	A0_1= A.subs({alpha:-pi/2, a:0, theta:theta1, d:420}) #0.3556
 
-	A1_2= A.subs({alpha:-pi/2, a:0, theta:theta2, d:0})
+	A1_2= A.subs({alpha:0, a:690, theta:theta2, d:0})
 	A1_2=nsimplify(A1_2,tolerance=1e-3,rational=True)
 
-	A2_3= A.subs({alpha:-pi/2, a:0, theta:theta3, d:520.7}) #0.5207
+	A2_3= A.subs({alpha:0, a:690, theta:theta3, d:0}) #0.5207
 	A2_3=nsimplify(A2_3,tolerance=1e-3,rational=True)
 
-	A3_4= A.subs({alpha:pi/2, a:0, theta:-theta4, d:0})
+	A3_4= A.subs({alpha:pi/2, a:175, theta:-theta4, d:50})
 	A3_4=nsimplify(A3_4,tolerance=1e-3,rational=True)
 
-	A4_5= A.subs({alpha:pi/2, a:0, theta:theta5, d:520.7+523.24}) #1.04394
+	A4_5= A.subs({alpha:0, a:0, theta:theta5, d:700}) #1.04394
 	A4_5=nsimplify(A4_5,tolerance=1e-3,rational=True)
 
 	A5_6= A.subs({alpha:0, a:0, theta:theta6, d:0})
@@ -108,36 +109,39 @@ if __name__=="__main__":
 
 	T0_6=(T0_5*A5_6)
 	
-	T=T0_6
+	T=T0_5
 	#xd,yd,zd=0,0,1920.24
-	xd,yd,zd=1043.939,0,876.3
-	Q=np.zeros(6)
-	Q=np.array([0,0,0,0,0,0])
+	xd,yd,zd=1000,1000,0
+	Q=np.zeros(5)
+	Q=np.array([0,pi/2,0,0,0])
 	#print(Q)
-	Ta=T.subs({theta1:Q[0],theta2:Q[1],theta3:Q[2],theta4:Q[3],theta5:Q[4],theta6:Q[5]})
+	Ta=T.subs({theta1:Q[0],theta2:Q[1],theta3:Q[2],theta4:Q[3],theta5:Q[4]})
 	Ta=nsimplify(Ta,tolerance=1e-3,rational=True)
 	Ta=np.array(Ta,dtype=float)
 	xc,yc,zc=Ta[0,3],Ta[1,3],Ta[2,3]
-	#print(xc)
+	print("TF =",Ta)
 	f=T[:3,3]
-	X=[theta1,theta2,theta3,theta4,theta5,theta6]
+	X=[theta1,theta2,theta3,theta4,theta5]
 	J_half=f.jacobian(X)
-	J_otherhalf=T0_1[:3,2].row_join(T0_2[:3,2].row_join(T0_3[:3,2].row_join(T0_4[:3,2].row_join(T0_5[:3,2].row_join(T0_6[:3,2])))))
+	J=J_half.subs({theta1:Q[0],theta2:Q[1],theta3:Q[2],theta4:Q[3],theta5:Q[4]})
+	
+	J_otherhalf=T0_1[:3,2].row_join(T0_2[:3,2].row_join(T0_3[:3,2].row_join(T0_4[:3,2].row_join(T0_5[:3,2]))))
 	J=J_half.col_join(J_otherhalf)
-	J=J.subs({theta1:Q[0],theta2:Q[1],theta3:Q[2],theta4:Q[3],theta5:Q[4],theta6:Q[5]})
+	J=J.subs({theta1:Q[0],theta2:Q[1],theta3:Q[2],theta4:Q[3],theta5:Q[4]})
 	J=nsimplify(J,tolerance=1e-3,rational=True)
 	J=np.array(J,dtype=float)
+	print("JF =",J)
 	qd=getqdot(J,xc,yc,zc,xd,yd,zd)
 	Q=Q+qd
-	#print(qd)
+	#print()
 	while (1):
-		T=T0_6
-		Ta=T.subs({theta1:Q[0,0],theta2:Q[0,1],theta3:Q[0,2],theta4:Q[0,3],theta5:Q[0,4],theta6:Q[0,5]})
+		T=T0_5
+		Ta=T.subs({theta1:Q[0,0],theta2:Q[0,1],theta3:Q[0,2],theta4:Q[0,3],theta5:Q[0,4]})
 		Ta=nsimplify(Ta,tolerance=1e-3,rational=True)
 		
 		Ta=np.array(Ta,dtype=float)
 		xc,yc,zc=Ta[0,3],Ta[1,3],Ta[2,3]
-		print(str(xc) + " " + str(yc) + " " + str(zc)+ " " + str(Ta[0,0]))
+		#print(str(xc) + " " + str(yc) + " " + str(zc)+ " " + str(Ta[0,0]))
 		if (xc==xd and yc==yd and zc==zd):
 			print("BREAK")
 			break;
@@ -146,15 +150,15 @@ if __name__=="__main__":
 		qd=getqdot(J,xc,yc,zc,xd,yd,zd)
 		Q=Q+qd
 	
-	#for i in range(0,6):
-		#Q[0,i]=rnd(Q[0,i])
+	for i in range(0,5):
+		Q[0,i]=rnd(Q[0,i])
 	
 	pri(Q)
-	print(Q)
-	T0_w=T0_6.subs({theta1:0,theta2:0,theta3:0,theta4:pi/2,theta5:0,theta6:0})
+	#print(Q)
+	T0_w=T0_5.subs({theta1:0,theta2:0,theta3:0,theta4:pi/2,theta5:0,theta6:0})
 	T0_w=nsimplify(T0_w,tolerance=1e-3,rational=True)
-	print(T0_w)
-	T0_w=T0_6.subs({theta1:Q[0,0],theta2:Q[0,1],theta3:Q[0,2],theta4:Q[0,3],theta5:Q[0,4],theta6:Q[0,5]})
+	#print(T0_w)
+	T0_w=T0_5.subs({theta1:Q[0,0],theta2:Q[0,1],theta3:Q[0,2],theta4:Q[0,3],theta5:Q[0,4]})
 	T0_w=nsimplify(T0_w,tolerance=1e-3,rational=True)
 	print(T0_w)
 	
